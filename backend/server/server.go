e846f5b0a6fd88abd7b927e36f8d45783a2c3c9b
Sunthorn Kompita <sunthorn.ko@ku.th>
2025-04-04 17:17:43 +0700

    Enable CORS on the backend to allows request from frontend

--- backend/server/server.go
+++ backend/server/server.go
@@ -4,7 +4,9 @@ import (
 	"crops-recommendation/backend/database"
 	"crops-recommendation/backend/route"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"log"
+	"time"
 )
 
 func Startserver(){
@@ -12,11 +14,19 @@ func Startserver(){
 	if err != nil{
 		log.Fatal("Failed to connect to database:", err)
 	}
-	
+
 	defer db.Close()
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3001"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour, // Cache preflight request
+	}))
+
 	route.SetupRouter(router, db)
+
 	router.Run("localhost:8080")
-
 }
-
